45c621cd6a507d1f9e7b285085e8c2c554fc4354
helmimuzkr <mhelmimuzakir@gmail.com>
2024-09-01 00:05:39 +0700

    display all dot files

--- nvim/lua/plugins/treesitter/inittreesitter.lua
+++ nvim/lua/plugins/treesitter/inittreesitter.lua
@@ -1,6 +1,21 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "go", "gomod", "gowork", "gosum" } },
+    opts = {
+      ensure_installed = { "go", "gomod", "gowork", "gosum" },
+    },
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      }
+    }
+  }
 }
