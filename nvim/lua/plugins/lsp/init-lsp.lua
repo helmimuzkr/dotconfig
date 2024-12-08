4430b132ab2f5ef21ec2566631b4b0165abdf8a7
helmimuzkr <mhelmimuzakir@gmail.com>
2024-12-09 01:00:23 +0700

    setup nvim for clang

--- nvim/lua/plugins/lsp/init-lsp.lua
+++ nvim/lua/plugins/lsp/init-lsp.lua
@@ -2,7 +2,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = { gopls = require("plugins.lsp.gopls") }
+      servers = {
+        gopls = require("plugins.lsp.gopls"),
+        clangd = require("plugins.lsp.clangd"),
+      },
     },
   },
 }
